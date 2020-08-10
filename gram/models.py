136c0cf9a527efdd111456a15477ae9e609f70be
ivine369 <ivinekemuma014@gmail.com>
2020-08-10 16:56:22 +0300

    add files

---
+++ gram/models.py
@@ -0,0 +1,38 @@
+ 
+from django.db import models
+from django.contrib.auth.models import User
+import datetime
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=350) 
+    profile_pic = models.ImageField(upload_to='ProfilePic/')
+    profile_avatar = models.ImageField(upload_to='Avators/')
+    date = models.DateTimeField(auto_now_add=True, null= True)  
+
+    def __str__(self):
+        return self.profile.user
+    
+    
+class Image(models.Model):
+    image = models.ImageField(upload_to ='pictsagram/')
+    image_caption = models.CharField(max_length=700)
+    tag_someone = models.CharField(max_length=50,blank=True)
+    imageuploader_profile = models.ForeignKey(User, on_delete=models.CASCADE,null='True', blank=True)
+    image_likes = models.ManyToManyField('Profile', default=False, blank=True, related_name='likes')
+    date = models.DateTimeField(auto_now_add=True, null= True)
+
+    def __str__(self):
+        return self.image_caption
+    
+class Comments (models.Model):
+    comment_post = models.CharField(max_length=150)
+    author = models.ForeignKey('Profile',related_name='commenter' , on_delete=models.CASCADE)
+    commented_image = models.ForeignKey('Image', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author
