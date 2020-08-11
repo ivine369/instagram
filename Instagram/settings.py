9f0a9fd69d08d08237a225594503bf56372c6143
ivine369 <ivinekemuma014@gmail.com>
2020-08-11 14:50:49 +0300

    make changes

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -80,10 +80,12 @@ WSGI_APPLICATION = 'Instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER':'kiiru',
-        'PASSWORD':'Access',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'os.environ.get('DB_NAME')',
+        'USER': 'os.environ.get('DB_USER')
+        'PASSWORD': 'os.environ.get('DB_PASSWORD')
+        'HOST':'os.environ.get('DB_HOST')
+        'PORT': 'os.environ.get('DB_PORT')
     }
 }
 
